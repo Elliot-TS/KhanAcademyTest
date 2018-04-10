dd2df37cbe54ce4d2a6eb4d3d9b52298380b22f8
RingOfProgrammers <34894078+RingOfProgrammers@users.noreply.github.com>
2018-04-10 17:36:08 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
-var p = document.getElementById("p");
+var ex = (function(){return this;})();
+var doc = "document";
+var p = ex[doc].getElementById("p");
 console.log("Hello?");
 p.innerHTML = "hello";
-var el = document.createElement("p");
+var el = ex[doc].createElement("p");
 el.innerText = "So, I can now connect to GitHub.  Later, I will see if I can write to github";
-document.body.appendChild(el);
+ex[doc].body.appendChild(el);

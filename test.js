4a2ffbccf3268e0c7c2c2596d8caae1f9034641d
RingOfProgrammers <34894078+RingOfProgrammers@users.noreply.github.com>
2018-04-10 11:25:58 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,6 @@
 var p = document.getElementById("p");
 console.log("Hello?");
 p.innerHTML = "hello";
+var el = document.createElement("p");
+el.innerText = "So, I can now connect to GitHub.  Later, I will see if I can write to github";
+document.body.appendChild(el);
